cbb1f27fe0343e40c8486779d325f9cd4ec8db42
Mingy123 <mingy123.coc@gmail.com>
2023-09-30 05:37:32 +0800

    vue example

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue';
+
+const n = ref(0)
+</script>
+
+<template>
+<body>
+  <h1>Hello there</h1>
+  <button @click="n+=1">{{ n }}</button>
+  <p v-if="n == 3">Cool</p>
+  <p v-else>Not cool</p>
+</body>
+</template>
+
+<style scoped>
+/* Scoped means only this file */
+body {
+  background-color: aqua;
+}
+</style>
