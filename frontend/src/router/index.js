2a1866b25bff1b92b5f5f598693bc7ed0a22cd1c
Mingy123 <mingy123.coc@gmail.com>
2023-10-01 16:19:21 +0800

    moved world map to its own page

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,14 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/Home.vue'
 import Resources from '../views/Resources.vue'
-import Shipping from '../views/Shipping.vue'
+import Map from '../views/Map.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', name: 'home', component: HomeView },
     { path: '/resources', name: 'resources', component: Resources },
-    { path: '/shipping', name: 'shipping', component: Shipping },
+    { path: '/map', name: 'World Map', component: Map },
   ]
 })
 
