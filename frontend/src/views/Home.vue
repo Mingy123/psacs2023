db94d74f379dbb79fb0c2173494a015686e315d0
Mingy123 <mingy123.coc@gmail.com>
2023-10-01 18:15:12 +0800

    need to leave now

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -141,6 +141,7 @@ function checkLogin() {
       <svg height="1em" viewBox="0 0 448 512"><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>
     </div>
     <h1>PORT++</h1>
+    <h4>Using big data and smart technologies to ensure resilience, agility and sustainability</h4>
     <div id="row-1">
       <div class="col_single">
         <h3>Throughput Forecast</h3>
@@ -369,6 +370,10 @@ body {
 h1 {
   font-family: Arial, Helvetica, sans-serif;
   font-size: 5em;
+  text-align: center;
+  margin-bottom: 12px;
+}
+h4 {
   text-align: center;
 }
 
